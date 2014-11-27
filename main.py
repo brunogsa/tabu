829fa75177ddc094e7569b7f77e9913a7f89b572
Fábio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2014-11-27 20:43:56 -0200

    Cálculo do budget

--- main.py
+++ main.py
@@ -35,19 +35,40 @@ def is_feasible_solution(villains, heroes, budget):
   return feasible
   
 def calculate_budget(villains, heroes, villains_team):
-  # TODO: Calcular como no slide
-  return 1000
+  # Exp 1
+  heroes_pg = heroes[["Intelligence", "Strength", "Speed", "Durability", "Energy Projection", "Fighting Skills"]].mean(1).values.mean()
+  villains_team_pg = villains_team[["Intelligence", "Strength", "Speed", "Durability", "Energy Projection", "Fighting Skills"]].mean(1).values.mean()
+  ratio_pg = heroes_pg/villains_team_pg
+  
+  heroes_pop = heroes["Number of Comic Books Where Character Appeared"].mean()
+  villains_team_pop = villains_team["Number of Comic Books Where Character Appeared"].mean()
+  ratio_pop = heroes_pop/villains_team_pop
+  
+  villains_team_pg = villains_team[["Intelligence", "Strength", "Speed", "Durability", "Energy Projection", "Fighting Skills"]].mean(1).values
+  villains_team_pop = villains_team["Number of Comic Books Where Character Appeared"].values
+  vt_cost = (villains_team_pg*villains_team_pop).sum()
+  
+  exp1 = ratio_pg*ratio_pop*vt_cost
+  
+  # Exp 2
+  villains_pg = villains[["Intelligence", "Strength", "Speed", "Durability", "Energy Projection", "Fighting Skills"]].mean(1).values.mean()
+  factor = villains_team_pg.mean()/villains_pg
+
+  exp2 = factor*heroes_pg*heroes_pop*len(villains_team)
+  
+  return max(exp1, exp2)
 
 def main():
   villains_ids = np.fromfile(sys.argv[1], dtype=int, sep=" ")
 
   df = pd.read_csv(config.CHARACTERS_CSV, sep=';')
-  villains = df.loc[df['Hero or Villain'] == 'hero']
+  villains = df.loc[df['Hero or Villain'] == 'villain']
   heroes = df.loc[df['Hero or Villain'] == 'hero']
   villains_team = df.loc[df["Character ID"].isin(villains_ids)]
   budget = calculate_budget(villains, heroes, villains_team)
-  heroes_team = construct_initial_solution(villains, heroes, villains_team, budget)
-  print "Heroes team:"
-  print heroes_team
+  print budget
+  #heroes_team = construct_initial_solution(villains, heroes, villains_team, budget)
+  #print "Heroes team:"
+  #print heroes_team
 
 main()
