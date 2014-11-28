a6ebbfea1f618c0704f8fc2acb9ab6a7cda0bb99
Fabio Beranizo <fabio.beranizo@gmail.com>
2014-11-28 21:24:25 -0200

    Formatação do código e comentários

--- main.py
+++ main.py
@@ -6,57 +6,69 @@ import random
 import numpy as np
 import pandas as pd
 
+POWERGRID = ["Intelligence", "Strength", "Speed", "Durability", "Energy Projection", "Fighting Skills"]
+POPULARITY = "Number of Comic Books Where Character Appeared"
 
-# 1a versão: Buscar uma solução viável qualquer
+# Versão 1: Sortear soluções e verificar se é viável
 def construct_initial_solution(villains, heroes, villains_team, budget):
+  # Variável 'attempt' criada para evitar loop infinito
   attempt = 0
   while attempt < 50:
     attempt += 1
     heroes_team = heroes.loc[random.sample(heroes.index, len(villains_team))]
-    if is_feasible_solution(villains_team, heroes_team, budget):
+    if is_viable_solution(villains_team, heroes_team, budget):
       break
-  print str(attempt) + " attempts"
+  
   return heroes_team
   
+# Verifica se uma solução é viável dados um time de vilões, 
+# um time de heróis e um orçamento disponível
+def is_viable_solution(villains_team, heroes_team, budget):
+  # Não é solução viável se o time de heróis é MAIOR que o de vilões
+  if len(heroes_team) > len(villains_team): return False
   
-def is_feasible_solution(villains_team, heroes_team, budget):
-  # O número de heróis escolhidos é menor ou igual ao de vilões
-  feasible = len(heroes_team) <= len(villains_team)
-  if not feasible: return False
+  # CALCULAR CUSTO DO TIME
+  # CUSTO: SUM(Popularidade[h]*PowegridMedio[h]) para cada herói h
   
-  # O custo" dos heróis escolhidos é menor ou igual ao "orçamento" disponível, 
-  # sendo o custo: o somatório de Popularidade[h]*PowegridMedio[h] para os h heróis do time.
-  heroes_team_pg = heroes_team[["Intelligence", "Strength", "Speed", "Durability", "Energy Projection", "Fighting Skills"]].mean(1).values
-  heroes_team_pop = heroes_team["Number of Comic Books Where Character Appeared"].values
+  # Para cada herói, calcula a média de suas hablidades (powergrid médio)
+  heroes_team_pg = heroes_team[POWERGRID].mean(1).values
+
+  # Para cada herói, seleciona sua popularidade
+  heroes_team_pop = heroes_team[POPULARITY].values
+  
+  # Multiplica powergrid médio e popularidade e soma os resultados para obter custo
   heroes_cost = (heroes_team_pg*heroes_team_pop).sum()
-  feasible &= budget >= heroes_cost
-  if not feasible: return False
-
-  # A média das habilidades dos heróis escolhidos é maior que a média dos vilões
-  villains_team_pg = villains_team[["Intelligence", "Strength", "Speed", "Durability", "Energy Projection", "Fighting Skills"]].mean(1).values
-
-  feasible &= heroes_team_pg.mean() > villains_team_pg.mean()
   
-  return feasible
+  # Não é solução viável se o custo do time de heróis é maior que o orçamento
+  if heroes_cost > budget: return False
+
+  # Para cada vilão, calcula a média de suas hablidades (powergrid médio)
+  villains_team_pg = villains_team[POWERGRID].mean(1).values
+
+  # Não é solução viável se a média das habilidades dos vilões é MAIOR que a média dos heróis
+  if villains_team_pg.mean() > heroes_team_pg.mean(): return False
   
+  return True
+
+# Calcula orçamento disponível, ver slide enviado pelo professor
 def calculate_budget(villains, heroes, villains_team):
   # Exp 1
-  heroes_pg = heroes[["Intelligence", "Strength", "Speed", "Durability", "Energy Projection", "Fighting Skills"]].mean(1).values.mean()
-  villains_team_pg = villains_team[["Intelligence", "Strength", "Speed", "Durability", "Energy Projection", "Fighting Skills"]].mean(1).values.mean()
+  heroes_pg = heroes[POWERGRID].mean(1).values.mean()
+  villains_team_pg = villains_team[POWERGRID].mean(1).values.mean()
   ratio_pg = heroes_pg/villains_team_pg
   
-  heroes_pop = heroes["Number of Comic Books Where Character Appeared"].mean()
-  villains_team_pop = villains_team["Number of Comic Books Where Character Appeared"].mean()
+  heroes_pop = heroes[POPULARITY].mean()
+  villains_team_pop = villains_team[POPULARITY].mean()
   ratio_pop = heroes_pop/villains_team_pop
   
-  villains_team_pg = villains_team[["Intelligence", "Strength", "Speed", "Durability", "Energy Projection", "Fighting Skills"]].mean(1).values
-  villains_team_pop = villains_team["Number of Comic Books Where Character Appeared"].values
+  villains_team_pg = villains_team[POWERGRID].mean(1).values
+  villains_team_pop = villains_team[POPULARITY].values
   vt_cost = (villains_team_pg*villains_team_pop).sum()
   
   exp1 = ratio_pg*ratio_pop*vt_cost
   
   # Exp 2
-  villains_pg = villains[["Intelligence", "Strength", "Speed", "Durability", "Energy Projection", "Fighting Skills"]].mean(1).values.mean()
+  villains_pg = villains[POWERGRID].mean(1).values.mean()
   factor = villains_team_pg.mean()/villains_pg
 
   exp2 = factor*heroes_pg*heroes_pop*len(villains_team)
@@ -70,6 +82,7 @@ def main():
   villains = df.loc[df['Hero or Villain'] == 'villain']
   heroes = df.loc[df['Hero or Villain'] == 'hero']
   villains_team = df.loc[df["Character ID"].isin(villains_ids)]
+  
   budget = calculate_budget(villains, heroes, villains_team)
   heroes_team = construct_initial_solution(villains, heroes, villains_team, budget)
   
