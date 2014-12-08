2655411e2b358eca1d0c3ab5b28e4812f52420ad
Rodrigo Elizeu Goncalves <rodrigoegoncalves@gmail.com>
2014-12-08 17:02:12 -0200

    Version working, but slow

--- main.py
+++ main.py
@@ -6,40 +6,75 @@ import random
 import numpy as np
 import pandas as pd
 
-MAX_ITERATIONS_WITHOUT_IMPROVEMENT = 20
+MAX_ITERATIONS_WITHOUT_IMPROVEMENT = 100
 POWERGRID = ["Intelligence", "Strength", "Speed", "Durability", "Energy Projection", "Fighting Skills"]
 POPULARITY = "Number of Comic Books Where Character Appeared"
+COLLABORATION_LEVEL = "Number of Comic Books Where Character 1 and Character 2 Both Appeared"
 
-# Versão 1: Sortear soluções e verificar se é viável
-def construct_initial_solution(villains, heroes, villains_team, budget):
-  # Variável 'attempt' criada para evitar loop infinito
-  attempt = 0
-  while attempt < 50:
-    attempt += 1
-    heroes_team = heroes.loc[random.sample(heroes.index, len(villains_team))]
-    if is_viable_solution(villains_team, heroes_team, budget):
-      break
-  
+
+def construct_solution(initial_heroes_team, exclusion_list, villains_team, budget, villains, heroes, collaboration):
+  # tamanho maximo do time de herois
+
+  # TODO make a list with only heroes collaboration
+
+  team_max_size = len(villains_team)
+
+  heroes_team = initial_heroes_team
+
+  # obter os herois para a solucao inicial
+  # obtem um par de herois que tem a colaboracao maxima
+  if len(heroes_team) == 0:
+    tmp = collaboration.loc[-collaboration["Character 1 ID"].isin(villains["Character ID"]) & -collaboration["Character 2 ID"].isin(villains["Character ID"])]
+    max_collaboration = tmp.loc[tmp[COLLABORATION_LEVEL].argmax()]
+    heroes_team = heroes.loc[heroes["Character ID"].isin([max_collaboration["Character 1 ID"]]) | heroes["Character ID"].isin([max_collaboration["Character 2 ID"]])]
+    exclusion_list.append(heroes_team[1:2]["Character ID"].values[0])
+
+  # enquanto nao encontrar uma solucao viavel, continua procurando
+  while not is_viable_solution(villains_team, heroes_team, budget):
+    # remove um heroi da lista caso nao for solucao viavel
+    exclusion_list.append(heroes_team[0:1]["Character ID"].values[0])
+    heroes_team = heroes_team[1:]
+
+    # enquanto nao tiver o tamanho maximo de time
+    while len(heroes_team) < team_max_size:
+      # condicao de parada para solucao inviavel
+      if len(heroes_team) + len(exclusion_list) > len(heroes):
+        return None
+
+      random_hero = heroes_team.loc[random.sample(heroes_team.index, 1)]
+      tmp = collaboration.loc[collaboration["Character 1 ID"].isin([random_hero["Character ID"].values[0]])]
+      tmp = tmp.loc[-tmp["Character 2 ID"].isin(exclusion_list)]
+      tmp = tmp.loc[-tmp["Character 2 ID"].isin(villains["Character ID"])]
+      max_collaboration = tmp.loc[tmp[COLLABORATION_LEVEL].argmax()]
+      hero_max_colaboration_level = heroes.loc[heroes["Character ID"].isin([max_collaboration["Character 2 ID"]])]
+      heroes_team = heroes_team.append(hero_max_colaboration_level)
+      exclusion_list.append(hero_max_colaboration_level["Character ID"].values[0])
+
   return heroes_team
+
+# Versão 1: considerando usar a mesma funcao do tabu search
+def construct_initial_solution(villains_team, budget, villains, heroes, collaboration):
+  initial_heroes_team = pd.DataFrame(columns=heroes.columns.values)
+  return construct_solution(initial_heroes_team, [], villains_team, budget, villains, heroes, collaboration)
   
 # Verifica se uma solução é viável dados um time de vilões, 
 # um time de heróis e um orçamento disponível
 def is_viable_solution(villains_team, heroes_team, budget):
   # Não é solução viável se o time de heróis é MAIOR que o de vilões
   if len(heroes_team) > len(villains_team): return False
-  
+
   # CALCULAR CUSTO DO TIME
   # CUSTO: SUM(Popularidade[h]*PowegridMedio[h]) para cada herói h
-  
+
   # Para cada herói, calcula a média de suas hablidades (powergrid médio)
   heroes_team_pg = heroes_team[POWERGRID].mean(1).values
 
   # Para cada herói, seleciona sua popularidade
   heroes_team_pop = heroes_team[POPULARITY].values
-  
+
   # Multiplica powergrid médio e popularidade e soma os resultados para obter custo
   heroes_cost = (heroes_team_pg*heroes_team_pop).sum()
-  
+
   # Não é solução viável se o custo do time de heróis é maior que o orçamento
   if heroes_cost > budget: return False
 
@@ -82,16 +117,16 @@ def collaboration_level(collaboration, heroes_team, villains_team):
   c_level = 0
   c_heroes = collaboration.loc[(collaboration["Character 1 ID"].isin(heroes_ids)) & (collaboration["Character 2 ID"].isin(heroes_ids))]
   if not c_heroes.empty:
-    c_level += c_heroes["Number of Comic Books Where Character 1 and Character 2 Both Appeared"].sum()/2
-  
+    c_level += c_heroes[COLLABORATION_LEVEL].sum()/2
+
   c_villains = collaboration.loc[(collaboration["Character 1 ID"].isin(heroes_ids)) & (collaboration["Character 2 ID"].isin(villains_team))]
   if not c_villains.empty:
-    c_level += c_villains["Number of Comic Books Where Character 1 and Character 2 Both Appeared"].sum()
+    c_level += c_villains[COLLABORATION_LEVEL].sum()
   
   return c_level
 
 
-def tabu_search(villains, heroes, villains_team, collaboration, budget):
+def tabu_search(villains, heroes, villains_team, collaboration, budget, max_tabu, max_candidates):
   # Inserir na busca local uma lista de movimentos tabu que
   # impedem, por algumas iterações, que um determinado
   # movimento seja realizado.
@@ -103,47 +138,58 @@ def tabu_search(villains, heroes, villains_team, collaboration, budget):
   # Os parâmetros α e β são fixados a priori.
   # Parâmetros a ajustar: tamanho da lista tabu t, α e β.
 
-  gl_solution = -1 # Melhor solução global
+  # Constrói solução inicial
+  gl_solution = {}
+  gl_solution["team"] = construct_initial_solution(villains_team, budget, villains, heroes, collaboration)
+  gl_solution["collaboration_level"] = collaboration_level(collaboration, gl_solution["team"], villains_team) # Melhor solução global
   gl_last_improvement = 0 # Iterações desde a última melhoria
+  tabu_list = [] #lista tabu
   while gl_last_improvement < MAX_ITERATIONS_WITHOUT_IMPROVEMENT:
-    # Contrói solução inicial
-    heroes_team = construct_initial_solution()
-    lc_solution = collaboration_level(collaboration, heroes_team, villains_team)
-    lc_last_improvement = 0
-    
-    while lc_last_improvement < MAX_ITERATIONS_WITHOUT_IMPROVEMENT:
-      neighboor_solutions = []
-      # TODO: Gera soluções vizinhas (levando o tabu em consideração)
-      # TODO: Encontra a melhor solução e faz dela a atual
-      # TODO: Coloca no tabu os movimentos realizados para chegar na solução atual
-      
-      curr_solution = -1
-      if curr_solution > lc_solution:
-        lc_solution = curr_solution
-        lc_last_improvement = 0
-      else:
-        lc_last_improvement += 1
 
-    if lc_solution > gl_solution:
-      gl_solution = lc_solution
+
+    neighboor_solutions = []
+    initial_heroes_team = gl_solution["team"].loc[-gl_solution["team"]["Character ID"].isin(tabu_list)]
+    while len(neighboor_solutions) < max_candidates:
+      neighboor_solution = {}
+      neighboor_solution["team"] = construct_solution(initial_heroes_team, tabu_list, villains_team, budget, villains, heroes, collaboration)
+      neighboor_solution["collaboration_level"] = collaboration_level(collaboration, neighboor_solution["team"], villains_team)
+      neighboor_solutions.append(neighboor_solution)
+
+    neighboor_solutions.sort(key=lambda c: c["collaboration_level"], reverse=True)
+    best_candiate = neighboor_solutions[0]
+
+    if best_candiate["collaboration_level"] > gl_solution["collaboration_level"]:
+      gl_solution = best_candiate
       gl_last_improvement = 0
-    else: 
+      best_candiate["team"].sort(key=lambda c: c["collaboration_level"], reverse=True)
+      for hero in best_candiate["team"]:
+        if len(tabu_list) >= max_tabu:
+          tabu_list.pop(0)
+
+        tabu_list.append(hero)
+    else:
       gl_last_improvement += 1
+
+  return gl_solution["team"]
 
 
 def main():
   villains_ids = np.fromfile(sys.argv[1], dtype=int, sep=" ")
 
+
   df = pd.read_csv(config.CHARACTERS_CSV, sep=';')
   villains = df.loc[df['Hero or Villain'] == 'villain']
   heroes = df.loc[df['Hero or Villain'] == 'hero']
 
+  collaboration = pd.read_csv(config.SHARED_COMICS_CSV, sep=';')
+
   villains_team = df.loc[df["Character ID"].isin(villains_ids)]
   budget = calculate_budget(villains, heroes, villains_team)
-  heroes_team = construct_initial_solution(villains, heroes, villains_team, budget)
 
-  collaboration = pd.read_csv(config.SHARED_COMICS_CSV, sep=';')
-  
+  max_tabu = math.floor(len(villains_team))/2
+  max_candidates = len(villains_team)
+  heroes_team = tabu_search(villains, heroes, villains_team, collaboration, budget, max_tabu, max_candidates)
+
   print "Villains team:"
   print villains_team["Character Name"]
 
